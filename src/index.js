b4ca05946a2c9137c26dbe14af0b7e7424e8ef42
Oksana <110613334+OksanaKozak@users.noreply.github.com>
2023-05-17 11:30:26 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
@@ -8,3 +9,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+// ?---------------------
+const elem1 = <span>Hello</span>;
+const elem2 = <span>world</span>;
+const element = (<div>
+  {elem1}
+  {elem2}
+</div>);
+console.log(element);
+
+// ReactDOM.render(element, document.querySelector('#root'));
+
