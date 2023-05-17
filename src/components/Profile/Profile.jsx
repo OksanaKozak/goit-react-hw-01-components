ab9d500c8f17a8db5c9ccff6cac723bd2ebf5894
Oksana <110613334+OksanaKozak@users.noreply.github.com>
2023-05-17 14:19:52 +0300

    add

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,33 @@
+
+
+    const user = (
+      <div class="profile">
+      <div class="description">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2922/2922506.png"
+          alt="User avatar"
+          class="avatar"
+        />
+        <p class="name">Jacques Gluke</p>
+        <p class="tag">@jgluke</p>
+        <p class="location">Ocho Rios, Jamaica</p>
+      </div>
+    
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">1000</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">3000</span>
+        </li>
+      </ul>
+    </div>
+    );
+    console.log(user);
+    
